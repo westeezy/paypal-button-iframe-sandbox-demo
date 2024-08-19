e0428f5ca6019a647bb296890567c666e77113d8
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2024-08-19 13:25:51 -0500

    Update iframe url settings

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const mainExpressApp = { app: express(), port: mainPort };
 
 mainExpressApp.app.set("view engine", "ejs");
 
-let iframeHost = "https://fly.io/test";
+const iframeSettings = {
+  origin: "https://www.gregjopa.com",
+  url: "https://www.gregjopa.com/paypal-button-iframe-sandbox-demo/iframe.html"
+};
+
+let iframeHost = "https://gregjopa.com";
 
 if (process.env.ENVIRONMENT === "LOCAL") {
     const iframeExpressApp = { app: express(), port: 4000 };
     iframeExpressApp.app.use("/public", express.static("docs"));
-    iframeHost = `http://localhost:${iframeExpressApp.port}`;
+    iframeSettings.origin = `http://localhost:${iframeExpressApp.port}`;
+    iframeSettings.url = `http://localhost:${iframeExpressApp.port}/public/iframe.html`;
 
     iframeExpressApp.app.listen(iframeExpressApp.port, () => {
       console.log(`Static file server started at http://localhost:${iframeExpressApp.port}/`);
@@ -19,7 +25,7 @@ if (process.env.ENVIRONMENT === "LOCAL") {
 }
 
 mainExpressApp.app.get('/', (req, res) => {
-  res.render('index', { iframeHost });
+  res.render('index', { iframeSettings });
 });
 
 mainExpressApp.app.listen(mainExpressApp.port, () => {
