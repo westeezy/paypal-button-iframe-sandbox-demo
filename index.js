e1016bcaf8b20bdfbd20030ee78e8b4635caa38a
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2024-09-17 16:13:52 -0500

    Add new demo page

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 const express = require("express");
 
-
 const mainPort = process.env.EXPRESS_PORT ?? 3434;
 const mainExpressApp = { app: express(), port: mainPort };
 
 mainExpressApp.app.set("view engine", "ejs");
 
-const iframeSettings = {
-  origin: "https://www.gregjopa.com",
-  url: "https://www.gregjopa.com/paypal-button-iframe-sandbox-demo/iframe-v6.html"
-};
+const localhostIframeAppPort = 4000;
+
+function getIframeSettings(htmlPage = "iframe-v6.html") {
+  if (process.env.ENVIRONMENT === "LOCAL") {
+    return {
+      origin: `http://localhost:${localhostIframeAppPort}`,
+      url: `http://localhost:${localhostIframeAppPort}/public/${htmlPage}`
+    };
+  } else {
+    return {
+      origin: "https://www.gregjopa.com",
+      url: `https://www.gregjopa.com/paypal-button-iframe-sandbox-demo/${htmlPage}`
+    };
+  }
+}
 
 if (process.env.ENVIRONMENT === "LOCAL") {
-    const iframeExpressApp = { app: express(), port: 4000 };
-    iframeExpressApp.app.use("/public", express.static("docs"));
-    iframeSettings.origin = `http://localhost:${iframeExpressApp.port}`;
-    iframeSettings.url = `http://localhost:${iframeExpressApp.port}/public/iframe-v6.html`;
-
-    iframeExpressApp.app.listen(iframeExpressApp.port, () => {
-      // console.log(`Static file server started at http://localhost:${iframeExpressApp.port}/`);
-    });
+  const iframeExpressApp = { app: express(), port: localhostIframeAppPort };
+  iframeExpressApp.app.use("/public", express.static("docs"));
+  iframeExpressApp.app.listen(localhostIframeAppPort, () => {
+    // console.log(`Static file server started at http://localhost:${localhostIframeAppPort}/`);
+  });
 }
 
 mainExpressApp.app.get('/', (req, res) => {
-  res.render('index', { iframeSettings });
+  res.render('index', { iframeSettings: getIframeSettings("iframe-v6.html") });
 });
+
+mainExpressApp.app.get('/render-your-own-buttons', (req, res) => {
+  res.render('render-your-own-buttons', { iframeSettings: getIframeSettings("iframe-v6-flows-only.html") });
+});
+
 
 mainExpressApp.app.listen(mainExpressApp.port, () => {
   console.log(`Start here: http://localhost:${mainExpressApp.port}/`);
